580500d6067c73704bb52a4ef665b7855c292867
whirlwin <whirlwin90@gmail.com>
2012-01-29 14:39:02 +0100

    Cleaned and formatted the config classes.

--- src/main/java/no/niths/config/ApplicationConfig.java
+++ src/main/java/no/niths/config/ApplicationConfig.java
@@ -4,21 +4,21 @@ import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 @Configuration
 public class ApplicationConfig{
-	
 
-	@Bean
-	public static PropertyPlaceholderConfigurer properties(){
-		final PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-		final Resource[] resources = new ClassPathResource[ ] { new ClassPathResource( "persistence.properties")};
-		ppc.setLocations( resources );
-		ppc.setIgnoreUnresolvablePlaceholders( true );
-		
-		return ppc;
-	
-	}
-	
+    public static final String BASE_PACKAGE      = "no.niths",
+                               PERSISTENCE_PROPS = "persistence.properties";
+    @Bean
+    public static PropertyPlaceholderConfigurer properties(){
+        final PropertyPlaceholderConfigurer ppc =
+                new PropertyPlaceholderConfigurer();
+        final ClassPathResource[] resources = new ClassPathResource[] {
+                new ClassPathResource(PERSISTENCE_PROPS)};
+        ppc.setLocations(resources);
+        ppc.setIgnoreUnresolvablePlaceholders(true);
+
+        return ppc;
+    }
 }
--- src/main/java/no/niths/config/PersistenceHibernateConfig.java
+++ src/main/java/no/niths/config/PersistenceHibernateConfig.java
@@ -10,80 +10,78 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@ComponentScan({ "no.niths" }) //Change to DAO layer
-//@ImportResource( "classpath*:*springDataConfig.xml" )
+@ComponentScan({ApplicationConfig.BASE_PACKAGE})
 public class PersistenceHibernateConfig{
-	
-	@Value( "${jdbc.driver.class_name}" )
-	private String driverClassName;
 
-	@Value( "${jdbc.url}" )
-	private String url;
+    private final String DRIVER       = "jdbc.driver.class_name",
+                         URL          = "jdbc.url",
+                         USERNAME     = "jdbc.username",
+                         PASSWORD     = "jdbc.password",
+                         DIALECT      = "hibernate.dialect",
+                         HBM2DDL_AUTO = "hibernate.hbm2ddl.auto",
+                         SHOW_SQL     = "hibernate.show_sql";
 
-	@Value("${jdbc.username}")
-	private String username;
-	
-	@Value("${jdbc.password}")
-	private String password;
-	
-	@Value( "${hibernate.dialect}" )
-	private String hibernateDialect;
-	
-	@Value( "${hibernate.hbm2ddl.auto}" )
-	private String hibernateHbm2ddlAuto;
+    @Value("${" + DRIVER + "}")
+    private String driver;
 
-	@Value( "${hibernate.show_sql}" )
-	boolean hibernateShowSql;
+    @Value("${" + URL + "}")
+    private String url;
 
-	
+    @Value("${" + USERNAME + "}")
+    private String username;
 
-	
-	
-	//Add properties here, show sql and so on
-	final Properties additionlProperties(){
-		Properties prop = new Properties();
-		prop.setProperty("hibernate.hbm2ddl.auto", hibernateHbm2ddlAuto);
-		
-		return prop;
-	}
-	
+    @Value("${" + PASSWORD + "}")
+    private String password;
+
+    @Value("${" + DIALECT + "}")
+    private String hibernateDialect;
+
+    @Value("${" + HBM2DDL_AUTO + "}")
+    private String hbm2ddlAuto;
+
+    @Value("${" + SHOW_SQL + "}")
+    private Boolean showSQL;
+
+    final Properties additionlProperties(){
+        Properties props = new Properties();
+        props.setProperty(DIALECT, hibernateDialect);
+        props.setProperty(HBM2DDL_AUTO, hbm2ddlAuto);
+        props.setProperty(SHOW_SQL, showSQL.toString());
+
+        return props;
+    }
 
    @Bean
-   public org.springframework.orm.hibernate4.LocalSessionFactoryBean alertsSessionFactory(){
-		org.springframework.orm.hibernate4.LocalSessionFactoryBean sessionFactory = new org.springframework.orm.hibernate4.LocalSessionFactoryBean();
-      sessionFactory.setDataSource( this.restDataSource() );
-      sessionFactory.setPackagesToScan( new String[ ] { "no.niths" } );
-      sessionFactory.setHibernateProperties( this.additionlProperties() );
-     
-      
+   public LocalSessionFactoryBean alertsSessionFactory() {
+      LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+      sessionFactory.setDataSource(restDataSource());
+      sessionFactory.setPackagesToScan(ApplicationConfig.BASE_PACKAGE);
+      sessionFactory.setHibernateProperties(additionlProperties());
+
       return sessionFactory;
    }
+
    @Bean
    public DataSource restDataSource(){
       DriverManagerDataSource dataSource = new DriverManagerDataSource();
-      dataSource.setDriverClassName( this.driverClassName );
-      dataSource.setUrl( this.url );
-      dataSource.setUsername( this.username );
-      dataSource.setPassword( this.password );
-      
+      dataSource.setDriverClassName(driver);
+      dataSource.setUrl(url);
+      dataSource.setUsername(username);
+      dataSource.setPassword(password);
+
       return dataSource;
    }
-   
+
    @Bean
    public HibernateTransactionManager transactionManager(){
       HibernateTransactionManager txManager = new HibernateTransactionManager();
-      txManager.setSessionFactory( this.alertsSessionFactory().getObject() );
-      
+      txManager.setSessionFactory(alertsSessionFactory().getObject());
+
       return txManager;
    }
-   
-//   @Bean
-//   public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
-//      return new PersistenceExceptionTranslationPostProcessor();
-//   }
-
 }
