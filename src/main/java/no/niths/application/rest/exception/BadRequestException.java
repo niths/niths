4b1af59a24207fa57fecc2471b9d37dda5702b61
Bendik Rostad <bendikr@gmail.com>
2012-02-02 13:58:22 +0100

    Added badreqex

---
+++ src/main/java/no/niths/application/rest/exception/BadRequestException.java
@@ -0,0 +1,24 @@
+package no.niths.application.rest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus( value = HttpStatus.BAD_REQUEST )
+public final class BadRequestException extends RuntimeException{
+
+	private static final long serialVersionUID = 2962462423679006339L;
+	
+	public BadRequestException(){
+		super();
+	}
+	public BadRequestException( final String message, final Throwable cause ){
+		super( message, cause );
+	}
+	public BadRequestException( final String message ){
+		super( message );
+	}
+	public BadRequestException( final Throwable cause ){
+		super( cause );
+	}
+
+}
