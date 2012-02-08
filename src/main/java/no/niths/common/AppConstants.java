bdb4d4510c0365dfc54e50eabc01398e5970a514
Bendik Rostad <bendikr@gmail.com>
2012-02-08 15:48:31 +0100

    Added topic (One course consists of many topics)

--- src/main/java/no/niths/common/AppConstants.java
+++ src/main/java/no/niths/common/AppConstants.java
@@ -5,5 +5,6 @@ public final class AppConstants {
     public static final String COMMITTEES       = "committees",
                                COMMITTEE_EVENTS = "committee_events",
                                COURSES          = "courses",
-                               STUDENTS         = "students";
+                               STUDENTS         = "students",
+                               TOPICS			= "topics";
 }
