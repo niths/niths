e51707d78a8d8a97c3fcef145961179466c2a5d4
Andre Kristensen <andre.kristensen@lyse.net>
2012-02-10 10:40:35 +0100

    Removed a parameter in @contextparam

--- src/test/java/no/niths/services/StudentServiceTest.java
+++ src/test/java/no/niths/services/StudentServiceTest.java
@@ -21,7 +21,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { TestAppConfig.class, HibernateConfig.class }, loader = AnnotationConfigContextLoader.class)
+@ContextConfiguration(classes = { TestAppConfig.class, HibernateConfig.class })
 public class StudentServiceTest {
 
 	private static final Logger logger = LoggerFactory
