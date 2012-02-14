40631743553f7093d2178d3fd263bf9029e7f38f
Bendik Rostad <bendikr@gmail.com>
2012-02-14 14:46:02 +0100

    Changed so that getById soes not return empty committees and courses list

--- src/main/java/no/niths/services/StudentService.java
+++ src/main/java/no/niths/services/StudentService.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class StudentService {
-	
+
 	private static final Logger logger = LoggerFactory
 			.getLogger(StudentService.class);
 
@@ -31,52 +31,58 @@ public class StudentService {
 		Student s = repo.getById(id);
 
 		if (s != null) {
-			//s.getCommittees().size();
-			for (int i = 0; i < s.getCommittees().size(); i++){
-				s.getCommittees().get(i).setEvents(null);
+			if (s.getCommittees().size() > 0) {
+				s.setCommittees(null);
+			} else {
+				for (int i = 0; i < s.getCommittees().size(); i++) {
+					s.getCommittees().get(i).setEvents(null);
+				}
 			}
-			//s.getCourses().size();
-			for(int i = 0; i < s.getCourses().size(); i++){
-				s.getCourses().get(i).setTopics(null);
+			// s.getCourses().size();
+			if (s.getCourses().size() > 0) {
+				s.setCourses(null);
+			} else {
+				for (int i = 0; i < s.getCourses().size(); i++) {
+					s.getCourses().get(i).setTopics(null);
+				}
 			}
 		}
 
 		return s;
 	}
-	
-	
-//	/**
-//	 * 
-//	 * Returns all student with matching name.
-//	 * Splits the parameter into two pieces, first name and last name:
-//	 * 		
-//	 * 		Ex:
-//	 * 		Parameter name = "John Doe"
-//	 * 		First name = John
-//	 * 		Last name = Doe 
-//	 * 		
-//	 * 		Parameter name = "John J. Doe"
-//	 * 		First name = John
-//	 * 		Last name = J. Doe 
-//	 * 
-//	 * @param name The String containing the name
-//	 * @return List of all student matching the name
-//	 */
-//	public List<Student> getStudentByName(String name) {
-//		String[] fullName = name.trim().split(" ");
-//		Student stud = new Student();
-//		if (fullName.length > 1) {
-//			for (int i = 1; i < fullName.length; i++) {
-//				if (i != 1){
-//					fullName[i] += " ";
-//				}
-//				stud.setLastName(stud.getLastName() + fullName[i]);	
-//			}
-//		}
-//		stud.setFirstName(fullName[0]);
-//		logger.info("Search for students with name: " + stud.toString());
-//		return getAllStudents(stud);
-//	}
+
+	// /**
+	// *
+	// * Returns all student with matching name.
+	// * Splits the parameter into two pieces, first name and last name:
+	// *
+	// * Ex:
+	// * Parameter name = "John Doe"
+	// * First name = John
+	// * Last name = Doe
+	// *
+	// * Parameter name = "John J. Doe"
+	// * First name = John
+	// * Last name = J. Doe
+	// *
+	// * @param name The String containing the name
+	// * @return List of all student matching the name
+	// */
+	// public List<Student> getStudentByName(String name) {
+	// String[] fullName = name.trim().split(" ");
+	// Student stud = new Student();
+	// if (fullName.length > 1) {
+	// for (int i = 1; i < fullName.length; i++) {
+	// if (i != 1){
+	// fullName[i] += " ";
+	// }
+	// stud.setLastName(stud.getLastName() + fullName[i]);
+	// }
+	// }
+	// stud.setFirstName(fullName[0]);
+	// logger.info("Search for students with name: " + stud.toString());
+	// return getAllStudents(stud);
+	// }
 
 	public List<Student> getAllStudents() {
 		ArrayList<Student> temp = (ArrayList<Student>) repo.getAllStudents();
@@ -90,8 +96,8 @@ public class StudentService {
 	public List<Student> getAllStudents(Student s) {
 		ArrayList<Student> temp = (ArrayList<Student>) repo.getAllStudents(s);
 		for (int i = 0; i < temp.size(); i++) {
-//			temp.get(i).getCommittees().size();
-//			temp.get(i).getCourses().size();
+			// temp.get(i).getCommittees().size();
+			// temp.get(i).getCourses().size();
 			temp.get(i).setCommittees(null);
 			temp.get(i).setCourses(null);
 		}
