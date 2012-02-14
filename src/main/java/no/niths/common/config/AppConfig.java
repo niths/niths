b57944e64c3ba5b2f711a85324813def7bce11bd
whirlwin <whirlwin90@gmail.com>
2012-02-14 21:31:55 +0100

    Modified AppConfig to use classpath files.

--- src/main/java/no/niths/common/config/AppConfig.java
+++ src/main/java/no/niths/common/config/AppConfig.java
@@ -3,7 +3,7 @@ package no.niths.common.config;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class AppConfig {
@@ -12,15 +12,13 @@ public class AppConfig {
                                PERSISTENCE_PROPS = "persistence.properties";
     @Bean
     public static PropertyPlaceholderConfigurer properties(){
-    	 final PropertyPlaceholderConfigurer ppc =
-                 new PropertyPlaceholderConfigurer();
+        final PropertyPlaceholderConfigurer ppc =
+                new PropertyPlaceholderConfigurer();
+        final ClassPathResource[] resources = new ClassPathResource[] {
+                new ClassPathResource(PERSISTENCE_PROPS)};
+        ppc.setLocations(resources);
+        ppc.setIgnoreUnresolvablePlaceholders(true);
 
-    	 FileSystemResource fsr = new FileSystemResource("/usr/share/credentials/snith/persistence.properties");
-
-    	// Inncomment the line below if you are not a server. :)
-         // FileSystemResource fsr = new FileSystemResource(System.getenv("CREDENTIAL_PATH")+"/"+PERSISTENCE_PROPS);
-         ppc.setLocation(fsr);  
-         ppc.setIgnoreUnresolvablePlaceholders(true);
-         return ppc;
+        return ppc;
     }
 }
