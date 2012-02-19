5f79733992ae1930182ea5c5f4df3128033b14e3
Bendik Rostad <bendikr@gmail.com>
2012-02-19 12:40:35 +0100

    Added some comments

--- src/main/java/no/niths/common/config/SecurityConfig.java
+++ src/main/java/no/niths/common/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package no.niths.common.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-//@ComponentScan( "no.niths.security" )
+//@ComponentScan( "no.niths.application.rest" )
 @ImportResource( { "classpath*:springSecurityConfig.xml" } )
 public class SecurityConfig{
 	
