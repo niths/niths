d690a4a2d419b97cdccd1598388faafc3564ea2a
Bendik Rostad <bendikr@gmail.com>
2012-02-22 16:15:22 +0100

    Added javadoc

--- src/main/java/no/niths/application/rest/interfaces/GenericRESTController.java
+++ src/main/java/no/niths/application/rest/interfaces/GenericRESTController.java
@@ -1,16 +1,47 @@
 package no.niths.application.rest.interfaces;
 
 import java.util.ArrayList;
-
+/**
+ * Generic interface for Controllers
+ *
+ * @param <T> The type parameter
+ */
 public interface GenericRESTController<T> {
  
+	/**
+	 * Persists the domain
+	 * 
+	 * @param domain
+	 */
     public void create( T domain) ;
     
+    /**
+     * Returns the domain object with the given id
+     * 
+     * @param id the id of the domain object
+     * @return the domain object
+     */
     public T getById(Long id); 
 
+    /**
+     * Returns an arraylist with all domain objects of the type
+     * 
+     * @param domain the class type 
+     * @return List of all domain objects
+     */
     public ArrayList<T> getAll(T domain);
 
+    /**
+     * Updates the given domain object
+     * 
+     * @param domain the object to be updated
+     */
     public void update(T domain);
 
+    /**
+     * Deletes the domain object with the given id
+     * 
+     * @param id the if of the domain object to be deleted
+     */
     public void delete(Long id);
 }
