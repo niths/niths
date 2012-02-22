918cdd5dc70503c2c6044262736247dfbaf92d81
AndreKristensen <andre.kristensen@lyse.net>
2012-02-22 16:48:26 +0100

    changed eventdata to event in list

--- src/main/java/no/niths/application/rest/lists/CommitteeEventList.java
+++ src/main/java/no/niths/application/rest/lists/CommitteeEventList.java
@@ -9,25 +9,17 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import no.niths.common.AppConstants;
 import no.niths.domain.CommitteeEvent;
-import no.niths.domain.Event;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @XmlRootElement(name = AppConstants.EVENTS)
 @XmlAccessorType(XmlAccessType.FIELD)
 public class CommitteeEventList extends ListAdapter<CommitteeEvent>{
     private static final long serialVersionUID = 3045223036129251886L;
     
-    private static final Logger logger = LoggerFactory
-			.getLogger(CommitteeEventList.class);
-    
-    @XmlElement
+    @XmlElement(name="event")
     private List<CommitteeEvent> eventData;
 
     @Override
     public void setData(List<CommitteeEvent> data) {
-    	logger.info("Size is  " +data.size());
         this.eventData = data;
     }
     
