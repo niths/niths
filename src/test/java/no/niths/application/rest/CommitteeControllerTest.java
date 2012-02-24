299a50a26f64788bc5bff4709f8874276a10f2eb
whirlwin <whirlwin90@gmail.com>
2012-02-24 13:36:06 +0100

    Added C&R test in CommitteeControllerTest.

--- src/test/java/no/niths/application/rest/CommitteeControllerTest.java
+++ src/test/java/no/niths/application/rest/CommitteeControllerTest.java
@@ -1,63 +1,44 @@
 package no.niths.application.rest;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
+import no.niths.application.rest.interfaces.CommitteeController;
 import no.niths.common.config.HibernateConfig;
 import no.niths.common.config.TestAppConfig;
+import no.niths.domain.Committee;
 
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.web.client.RestTemplate;
 
 @Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { TestAppConfig.class, HibernateConfig.class })
-@TransactionConfiguration(transactionManager = "transactionManager")
+@TransactionConfiguration(
+        transactionManager = TestAppConfig.TRANSACTION_MANAGER)
 public class CommitteeControllerTest {
 
-	
+    @Autowired
+    private CommitteeController controller;
 
-	RestTemplate restTemplate = new RestTemplate();
-	
-	@Test
-	public void testAutowireRestTemplate(){
-		assertNotNull(restTemplate);
-	}
-	
-	@Test
-	public void testCreate() {
-		
-		
-	}
+    @Test
+    public void testCreateAndGetCommittee() {
+        int originalCount = controller.getAll(null).size();
+        
+        Committee firstCommittee = new Committee("qux", "corge");
+        controller.create(firstCommittee);
 
-	@Test
-	public void testGetById() {
-		HttpHeaders requestHeaders = new HttpHeaders();
-		requestHeaders.set("Accept", "application/xml");
-		HttpEntity<String> requestEntity = new HttpEntity<String>(requestHeaders);
-		HttpEntity<String> response = restTemplate.exchange("http://localhost:8080/niths/committees/1", HttpMethod.GET, requestEntity, String.class);
-		assertNotNull(response);
-	}
+        assertEquals(originalCount + 1, controller.getAll(null).size());
 
-	@Test
-	public void testGetAll() {
-//		fail("Not yet implemented");
-	}
+        Committee secondCommittee = controller.getAll(firstCommittee).get(0);
 
-	@Test
-	public void testUpdate() {
-//		fail("Not yet implemented");	
-	}
+        assertEquals(secondCommittee.getName(), firstCommittee.getName());
+    }
 
-	@Test
-	public void testDelete() {
-//		fail("Not yet implemented");	
-	}
-
+    public Committee getRandomCommittee() {
+        return new Committee("foo", "bar");
+    }
 }
