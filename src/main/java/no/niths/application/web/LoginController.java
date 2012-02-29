383cc95edea59985b9eccd5f6a5c5c92acc7dc12
AndreKristensen <andre.kristensen@lyse.net>
2012-02-29 16:50:59 +0100

    NITHS-179 added search otpion, not done yet.

--- src/main/java/no/niths/application/web/LoginController.java
+++ src/main/java/no/niths/application/web/LoginController.java
@@ -1,18 +1,43 @@
 package no.niths.application.web;
 
+import java.util.List;
+
+import no.niths.domain.Student;
+import no.niths.services.interfaces.StudentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping("admin")
 public class LoginController {
  
-	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView printWelcome() {
- 
-		return new ModelAndView("login").addObject("message",  "Spring 3 TEst");
- 
+	@Autowired
+	private StudentService serice;
+	
+	@RequestMapping(value ="all",method = RequestMethod.GET)
+	public ModelAndView getAllStudents(@RequestParam(value="query", required=false) String query){
+		ModelAndView view = new ModelAndView("index"); 
+		List<Student> students =null;
+		
+		
+		if((query == null)){
+			Student s = new Student();
+			s.setFirstName(query);
+			students=  serice.getAll(s);
+		}else{
+			students=  serice.getAll(null);
+		}
+
+		
+		view.addObject("studentList", students);
+		
+		return view;
 	}
+	
+	
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,30 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<html>
+<head><title>Admin panel</title></head>
+<body>
+	<h1>list of students</h1>
+	
+	
+	<form method="get" >
+		<input name="query" id="query" />
+		<input type="submit" value="søk" />
+
+	</form>
+	<table>
+		<tr>
+			<th>First Name</th>
+			<th>Last Name</th>
+			<th>Email</th>
+		</tr>
+		<c:forEach items="${studentList}" var="student">
+			<tr>
+				<td><c:out value="${student.firstName}" /></td>
+				<td><c:out value="${student.lastName}" /></td>
+				<td><c:out value="${student.email}" />)</td>
+			</tr>
+		</c:forEach>
+	</table>
+</body>
+</html>
