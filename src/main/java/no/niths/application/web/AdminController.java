a104d1b479f0cff8563b0230cde70c27096a91df
AndreKristensen <andre.kristensen@lyse.net>
2012-03-01 14:34:02 +0100

    NITHS-174 Renamed AdminController added more functionality to the view ++

--- src/main/java/no/niths/application/web/AdminController.java
+++ src/main/java/no/niths/application/web/AdminController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import no.niths.application.rest.CommitteeControllerImpl;
 import no.niths.domain.Student;
+import no.niths.domain.security.Role;
+import no.niths.services.interfaces.RoleService;
 import no.niths.services.interfaces.StudentService;
 
 import org.slf4j.Logger;
@@ -16,34 +18,56 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("admin")
-public class AdminWebController {
+public class AdminController {
 
 	
+	private static final String ADMIN = "admin";
+
 	private Logger logger = org.slf4j.LoggerFactory
 			.getLogger(CommitteeControllerImpl.class);
 	
 	@Autowired
 	private StudentService serice;
+	
+	@Autowired
+	private RoleService roles;
+	
+	private List<Role> listOfRoles;
+	
 
+	@RequestMapping(method =RequestMethod.POST)
+	public ModelAndView updateRoles(){
+		ModelAndView view = new ModelAndView(ADMIN);
+		logger.debug("hello" + " -----------------");
+		return view;
+	}
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView getAllStudents(
 			@RequestParam(value = "query", required = false, defaultValue="") String query) {
-		ModelAndView view = new ModelAndView("index");
+		ModelAndView view = new ModelAndView(ADMIN);
 		List<Student> students = null;
 
+	
+		
+		
 		logger.debug(query+ "  _--------------------------------------------------------");
 
 		if (!(query.equals(""))) {
 			Student s = new Student();
 			s.setFirstName(query);
-			students = serice.getAll(s);
+			students = serice.getStudentsAndRoles(s);
 		} else{
-			students = serice.getAll(null);
+			students = serice.getStudentsAndRoles(null);
 		}
 
+		getRolesSetSize();
 		view.addObject("studentList", students);
-
+		view.addObject("listOfRoles", listOfRoles);
 		return view;
 	}
-
+	
+	private void getRolesSetSize(){
+		listOfRoles = roles.getAll(null);
+	}
 }
