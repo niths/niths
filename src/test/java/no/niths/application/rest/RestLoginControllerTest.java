59d3b79627adc7817aa6add0b16a87a7ba7b09d5
Bendik Rostad <bendikr@gmail.com>
2012-03-10 11:57:41 +0100

    Added RestControllerTest (mocked GoogleAuthService)

---
+++ src/test/java/no/niths/application/rest/RestLoginControllerTest.java
@@ -0,0 +1,35 @@
+package no.niths.application.rest;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import no.niths.application.rest.auth.RestLoginControllerImpl;
+import no.niths.application.rest.interfaces.auth.RestLoginController;
+import no.niths.domain.security.Token;
+import no.niths.services.interfaces.auth.GoogleAuthenticationService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RestLoginControllerTest {
+	
+	@Mock
+	private GoogleAuthenticationService service;
+	
+	@InjectMocks
+	private RestLoginController controller = new RestLoginControllerImpl();
+	
+	@Test
+	public void testLogin(){
+		when(service.login("token")).thenReturn("returned");
+		assertEquals("returned", controller.login("token").getToken());
+		
+		Token t = new Token();
+		assertEquals(t.getToken(), controller.login(null).getToken());
+	}
+	
+
+}
