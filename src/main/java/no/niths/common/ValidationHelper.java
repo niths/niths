b10eb975ed354658e38f88f58bf00d503595be10
Bendik Rostad <bendikr@gmail.com>
2012-03-11 12:44:05 +0100

    Added method that check if an object passes bean validation(generic)

--- src/main/java/no/niths/common/ValidationHelper.java
+++ src/main/java/no/niths/common/ValidationHelper.java
@@ -1,6 +1,12 @@
 package no.niths.common;
 
 import java.util.ArrayList;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 import no.niths.application.rest.exception.ObjectNotFoundException;
 import no.niths.domain.Committee;
@@ -40,5 +46,16 @@ public class ValidationHelper {
 			throw new ObjectNotFoundException();
 		}
 		return false;
+	}
+	
+	//Returns true if the object has passed bean validation
+	public static <T> boolean hasObjectValidAttributes(T obj){
+		ValidatorFactory factory =
+		          Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<T>> constraintViolations =
+		           validator.validate(obj);
+		
+		return (constraintViolations.isEmpty());
 	}
 }
