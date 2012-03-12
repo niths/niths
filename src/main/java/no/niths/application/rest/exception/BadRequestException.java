4cadd40d34e09f8dc2ec49fa7bd4e774339e372b
whirlwin <whirlwin90@gmail.com>
2012-03-12 16:21:19 +0100

    Added space before curly brace.

--- src/main/java/no/niths/application/rest/exception/BadRequestException.java
+++ src/main/java/no/niths/application/rest/exception/BadRequestException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public final class BadRequestException extends RuntimeException{
+public final class BadRequestException extends RuntimeException {
 
     private static final long serialVersionUID = 2962462423679006339L;
 
