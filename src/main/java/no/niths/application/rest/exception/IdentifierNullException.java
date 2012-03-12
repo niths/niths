f44b2daff9f8850d73690f478891212a0993541d
whirlwin <whirlwin90@gmail.com>
2012-03-12 16:23:34 +0100

    Formatted identifierNullException.

--- src/main/java/no/niths/application/rest/exception/IdentifierNullException.java
+++ src/main/java/no/niths/application/rest/exception/IdentifierNullException.java
@@ -3,24 +3,28 @@ package no.niths.application.rest.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NO_CONTENT, reason = "The given object has a null identifier")
+@ResponseStatus(
+        value = HttpStatus.NO_CONTENT,
+        reason = "The given object has a null identifier")
 public class IdentifierNullException extends RuntimeException {
 
-	private static final long serialVersionUID = 4462886765420431421L;
+    private static final long serialVersionUID = 4462886765420431421L;
 
-	public IdentifierNullException() {
-		super();
-	}
+    public IdentifierNullException() {
+        super();
+    }
 
-	public IdentifierNullException(final String message, final Throwable cause) {
-		super(message, cause);
-	}
+    public IdentifierNullException(
+            final String message,
+            final Throwable cause) {
+        super(message, cause);
+    }
 
-	public IdentifierNullException(final String message) {
-		super(message);
-	}
+    public IdentifierNullException(final String message) {
+        super(message);
+    }
 
-	public IdentifierNullException(final Throwable cause) {
-		super(cause);
-	}
+    public IdentifierNullException(final Throwable cause) {
+        super(cause);
+    }
 }
