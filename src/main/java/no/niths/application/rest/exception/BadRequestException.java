9a8c9311b72bbf49b1067008d447f8282e18b6d5
whirlwin <whirlwin90@gmail.com>
2012-03-12 16:20:22 +0100

    Formatted class.

--- src/main/java/no/niths/application/rest/exception/BadRequestException.java
+++ src/main/java/no/niths/application/rest/exception/BadRequestException.java
@@ -3,22 +3,24 @@ package no.niths.application.rest.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus( value = HttpStatus.BAD_REQUEST )
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public final class BadRequestException extends RuntimeException{
 
-	private static final long serialVersionUID = 2962462423679006339L;
-	
-	public BadRequestException(){
-		super();
-	}
-	public BadRequestException( final String message, final Throwable cause ){
-		super( message, cause );
-	}
-	public BadRequestException( final String message ){
-		super( message );
-	}
-	public BadRequestException( final Throwable cause ){
-		super( cause );
-	}
+    private static final long serialVersionUID = 2962462423679006339L;
 
+    public BadRequestException(){
+        super();
+    }
+
+    public BadRequestException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public BadRequestException(final String message) {
+        super(message);
+    }
+
+    public BadRequestException(final Throwable cause) {
+        super(cause);
+    }
 }
