0d1158917d1ce79a519e856a9ec12253d6bd704d
Bendik Rostad <bendikr@gmail.com>
2012-03-14 12:12:06 +0100

    Added test of child + leader relat

--- src/test/java/no/niths/services/FadderGroupServiceTest.java
+++ src/test/java/no/niths/services/FadderGroupServiceTest.java
@@ -4,8 +4,10 @@ import no.niths.common.config.HibernateConfig;
 import no.niths.common.config.TestAppConfig;
 import no.niths.domain.APIEvent;
 import no.niths.domain.FadderGroup;
+import no.niths.domain.Student;
 import no.niths.services.interfaces.APIEventService;
 import no.niths.services.interfaces.FadderGroupService;
+import no.niths.services.interfaces.StudentService;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +26,9 @@ public class FadderGroupServiceTest {
 
 	@Autowired
 	private FadderGroupService fadderService;
+	
+	@Autowired
+	private StudentService studService;
 	
 	@Test
 	public void testCRUD(){
@@ -50,7 +55,96 @@ public class FadderGroupServiceTest {
 	
 	@Test
 	public void testLeadersRelationship(){
+		int studSize = studService.getAll(null).size();
+		FadderGroup g1 = new FadderGroup();
+		fadderService.create(g1);
+		Student s1 = new Student("ssss@sss.com");
+		Student s2 = new Student("sss2@sss.com");
+		Student s3 = new Student("sss3@sss.com");
+		studService.create(s1);
+		studService.create(s2);
+		studService.create(s3);
+		assertEquals(studSize + 3, studService.getAll(null).size());
 		
+		FadderGroup fetched = fadderService.getById(g1.getId());
+		fetched.getLeaders().add(s1);
+		fetched.getLeaders().add(s2);
+		fetched.getLeaders().add(s3);
+		fadderService.update(fetched);
+		
+		fetched = fadderService.getById(g1.getId());
+		assertEquals(3, fetched.getLeaders().size());
+		
+		//Remove a leader
+		fetched.getLeaders().remove(s1);
+		fadderService.update(fetched);
+		//Did it get removed?
+		fetched = fadderService.getById(g1.getId());
+		assertEquals(2, fetched.getLeaders().size());
+		//Student should still exist
+		assertEquals(studSize + 3, studService.getAll(null).size());
+		
+		//Delete a student
+		studService.hibernateDelete(s2.getId());
+		assertEquals(studSize + 2, studService.getAll(null).size());
+		//Should be removed from faddergroup
+		fetched = fadderService.getById(g1.getId());
+		assertEquals(1, fetched.getLeaders().size());
+		
+		//Delete the faddergroup
+		fadderService.hibernateDelete(g1.getId());
+		//Students should still exist
+		assertEquals(studSize + 2, studService.getAll(null).size());
+		
+		//Clean
+		studService.hibernateDelete(s1.getId());
+		studService.hibernateDelete(s3.getId());
+		assertEquals(studSize, studService.getAll(null).size());
 	}
 	
+	@Test
+	public void testChildrenRelationship(){
+		int studSize = studService.getAll(null).size();
+		FadderGroup g1 = new FadderGroup();
+		fadderService.create(g1);
+		Student s1 = new Student("ssss@sss.com");
+		Student s2 = new Student("sss2@sss.com");
+		Student s3 = new Student("sss3@sss.com");
+		studService.create(s1);
+		studService.create(s2);
+		studService.create(s3);
+		assertEquals(studSize + 3, studService.getAll(null).size());
+		
+		FadderGroup fetched = fadderService.getById(g1.getId());
+		fetched.getFadderChildren().add(s1);
+		fetched.getFadderChildren().add(s2);
+		fetched.getFadderChildren().add(s3);
+		fadderService.update(fetched);
+		
+		fetched = fadderService.getById(g1.getId());
+		assertEquals(3, fetched.getFadderChildren().size());
+		//Remove a child
+		fetched.getFadderChildren().remove(s1);
+		fadderService.update(fetched);
+		
+		fetched = fadderService.getById(g1.getId());
+		assertEquals(2, fetched.getFadderChildren().size());
+		//Student should still exist
+		assertEquals(studSize + 3, studService.getAll(null).size());
+		studService.hibernateDelete(s1.getId());
+		//Delete a child
+		studService.hibernateDelete(s2.getId());
+		assertEquals(studSize + 1, studService.getAll(null).size());
+		//Should be removed from group
+		fetched = fadderService.getById(g1.getId());
+		assertEquals(1, fetched.getFadderChildren().size());
+		
+		//Delete a group
+		fadderService.hibernateDelete(g1.getId());
+		//Student should not be deleted
+		assertEquals(studSize + 1, studService.getAll(null).size());
+		
+		studService.hibernateDelete(s3.getId());
+		assertEquals(studSize, studService.getAll(null).size());
+	}
 }
