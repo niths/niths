c388ae29bcb4d90e12cb493e218b1904baa529db
AndreKristensen <andre.kristensen@lyse.net>
2012-03-14 15:29:17 +0100

    Made serialize  transient

--- src/main/java/no/niths/domain/signaling/AccessPoint.java
+++ src/main/java/no/niths/domain/signaling/AccessPoint.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import no.niths.common.AppConstants;
@@ -17,6 +18,7 @@ import no.niths.common.AppConstants;
 @Table(name = AppConstants.ACCESS_POINTS)
 public class AccessPoint implements Serializable {
 
+	@Transient
 	private static final long serialVersionUID = 8118983219932188402L;
 
 	@Id
