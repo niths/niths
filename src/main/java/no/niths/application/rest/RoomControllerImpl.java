582df732358eb801077f496686889c365e94517d
whirlwin <whirlwin90@gmail.com>
2012-03-14 09:49:17 +0100

    Added temporary fix to RoomsControllerImpl.

--- src/main/java/no/niths/application/rest/RoomControllerImpl.java
+++ src/main/java/no/niths/application/rest/RoomControllerImpl.java
@@ -1,5 +1,7 @@
 package no.niths.application.rest;
 
+import java.util.ArrayList;
+
 import no.niths.application.rest.lists.ListAdapter;
 import no.niths.application.rest.lists.RoomList;
 import no.niths.common.AppConstants;
@@ -12,6 +14,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(AppConstants.ROOMS)
@@ -24,6 +28,30 @@ public class RoomControllerImpl extends AbstractRESTControllerImpl<Room> {
     private RoomService service;
 
     private RoomList roomList = new RoomList();
+
+    /**
+     * FIXME Figure out why there are duplicate rooms
+     */
+    @Override
+    @RequestMapping(
+            method  = RequestMethod.GET,
+            headers = RESTConstants.ACCEPT_HEADER)
+    @ResponseBody
+    public ArrayList<Room> getAll(Room room) {
+        roomList = (RoomList) super.getAll(room);
+        final int roomListSize = roomList.size();
+
+        // Temporary dirty fix to remove duplicates
+        for (int i = roomListSize - 1; i >= 1; i--) {
+            Room r = roomList.get(i);
+            
+            if (r.getId() == roomList.get(i - 1).getId()) {
+                roomList.remove(i);
+            }
+        }
+
+        return roomList;
+    }
 
     @Override
     public GenericService<Room> getService() {
