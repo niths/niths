2918c680b1aab6cf43507de3d3e188f936eee4d5
whirlwin <whirlwin90@gmail.com>
2012-03-15 21:32:15 +0100

    Fixed formatting in CourseRepositoryTest.

--- src/test/java/no/niths/infrastructure/CourseRepositoryTest.java
+++ src/test/java/no/niths/infrastructure/CourseRepositoryTest.java
@@ -32,75 +32,84 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class CourseRepositoryTest {
 
-    private static final Logger logger = LoggerFactory
-            .getLogger(CourseRepositoryTest.class);
+    @Autowired
+    private CourseRepository courseRepo;
 
     @Autowired
-    private CourseRepository repo;
-    
-    @Autowired
-    private SubjectRepository topicRepo;
-    
+    private SubjectRepository subjectRepo;
+
     @Test
-    public void testCreateATimeSchedule(){
-        Course c = new Course("Programmering", "Java and the like");
-//        c.setGrade(1);
-//        c.setTerm("Fall");
-        
-        Subject t1 = new Subject("Java innføring", "PG111", "Lær java", "10:00", "12:00");
-        t1.setWeekday("Monday");
-        topicRepo.create(t1);
-        Subject t2 = new Subject("Java viderekomne", "PG211", "Lær java", "12:00", "14:00");
-        t2.setWeekday("Monday");
-        topicRepo.create(t2);
-        
-        c.getSubjects().add(t1);
-        c.getSubjects().add(t2);
-        repo.create(c);
-        
-        assertEquals(2, repo.getById(c.getId()).getSubjects().size());
-    }
-    
+    public void testCreateCourseWithSubjects(){
 
-    
+        // Subject 1
+        Subject pg111 = new Subject(
+                "Java innføring",
+                "PG111",
+                "Lær java",
+                "10:00",
+                "12:00");
+        pg111.setWeekday("Monday");
+        subjectRepo.create(pg111);
+
+        // Subject 2
+        Subject pg211 = new Subject(
+                "Java viderekomne",
+                "PG211",
+                "Lær java",
+                "12:00",
+                "14:00");
+        pg211.setWeekday("Monday");
+        subjectRepo.create(pg211);
+
+        // Add both to a Course
+        Course course = new Course("Programmering", "Java and the like");
+        course.getSubjects().add(pg111);
+        course.getSubjects().add(pg211);
+        courseRepo.create(course);
+
+        assertEquals(
+                2,
+                courseRepo.getById(course.getId()).getSubjects().size());
+    }
+
     @Ignore
     @Test(expected=ConstraintViolationException.class)
     public void testWhenAddCourseWithSameName(){
-        int size = repo.getAll(null).size();
+        int size = courseRepo.getAll(null).size();
         Course c1 = new Course("XXX", "XXX");
 
-        repo.create(c1);
-        assertEquals(size + 1, repo.getAll(null).size());
+        courseRepo.create(c1);
+        assertEquals(size + 1, courseRepo.getAll(null).size());
         //Course with same name and grade
         //Should throw ConstraintViolationEx
         Course c2 = new Course("XXX", "XXX");
-        repo.create(c2);
+        courseRepo.create(c2);
     }
     
     
     @Test
     public void whenAddedTopics_CourseShouldHaveThem() {
-        int numTopics = topicRepo.getAll(null).size();
-        int numCourses = repo.getAll(null).size();
+        int numTopics = subjectRepo.getAll(null).size();
+        int numCourses = courseRepo.getAll(null).size();
 
         Course c1 = new Course("Programmering", "Java, c++");
         Subject t1 = new Subject();
         t1.setSubjectCode("PG111");
         c1.getSubjects().add(t1);
-        repo.create(c1);
+        courseRepo.create(c1);
         
         
-        assertEquals(numCourses + 1, repo.getAll(null).size());
-        assertEquals(numTopics + 1, topicRepo.getAll(null).size());
+        assertEquals(numCourses + 1, courseRepo.getAll(null).size());
+        assertEquals(numTopics + 1, subjectRepo.getAll(null).size());
         
-        Course res = repo.getById(c1.getId());
+        Course res = courseRepo.getById(c1.getId());
         int numOfTopics = res.getSubjects().size();
         if(numOfTopics > 0){
             res.getSubjects().remove(0);
-            repo.update(res);
+            courseRepo.update(res);
             
-            assertEquals(numOfTopics - 1, repo.getById(res.getId()).getSubjects().size() );
-            assertEquals(numTopics + 1, topicRepo.getAll(null).size());
+            assertEquals(numOfTopics - 1, courseRepo.getById(res.getId()).getSubjects().size() );
+            assertEquals(numTopics + 1, subjectRepo.getAll(null).size());
         }
         
         
@@ -108,95 +117,95 @@ public class CourseRepositoryTest {
     
     @Test(expected = IllegalArgumentException.class)
     public void whenInsertNull_persistenceShouldFail() {
-        repo.create(null);
+        courseRepo.create(null);
     }
     
     @Test
     public void whenInsertCourse_CourseShouldBePersisted(){
-        int size = repo.getAll(null).size();
+        int size = courseRepo.getAll(null).size();
         
         Course c = new Course("Name", "Desc");
-        repo.create(c);
+        courseRepo.create(c);
         
-        assertEquals(size + 1, repo.getAll(null).size());
+        assertEquals(size + 1, courseRepo.getAll(null).size());
     }
     
     
     
     @Test
     public void getCoursesByName_shouldReturnListWithCourses(){
-        int  size = repo.getAll(null).size();
+        int  size = courseRepo.getAll(null).size();
         Course c1 = new Course("One", "oneDesc");
         Course c2 = new Course("Two", "oneDesc");// equal desc
         Course c3 = new Course("Three", "threeDesc");
         Course c4 = new Course("Four", "fourDesc");
-        repo.create(c1);
-        repo.create(c2);
-        repo.create(c3);
-        repo.create(c4);
-        assertEquals(size + 4, repo.getAll(null).size());
+        courseRepo.create(c1);
+        courseRepo.create(c2);
+        courseRepo.create(c3);
+        courseRepo.create(c4);
+        assertEquals(size + 4, courseRepo.getAll(null).size());
         
         Course mockCourse= new Course();
         mockCourse.setName("One");
         
-        assertEquals(1, repo.getAll(mockCourse).size());
+        assertEquals(1, courseRepo.getAll(mockCourse).size());
         mockCourse.setName("Two");
-        assertEquals(1, repo.getAll(mockCourse).size());
+        assertEquals(1, courseRepo.getAll(mockCourse).size());
         mockCourse.setName("*AASDASD");
-        assertEquals(0, repo.getAll(mockCourse).size());
+        assertEquals(0, courseRepo.getAll(mockCourse).size());
     }
     
     @Test
     public void getCoursesByAttributes_shouldReturnListWithCourses(){
-        int  size = repo.getAll(null).size();
+        int  size = courseRepo.getAll(null).size();
         Course c1 = new Course("One", "oneDesc");
         Course c2 = new Course("Two", "oneDesc");// equal desc
         Course c3 = new Course("Three", "threeDesc");
         Course c4 = new Course("Four", "fourDesc");
-        repo.create(c1);
-        repo.create(c2);
-        repo.create(c3);
-        repo.create(c4);
+        courseRepo.create(c1);
+        courseRepo.create(c2);
+        courseRepo.create(c3);
+        courseRepo.create(c4);
         
-        assertEquals(size + 4, repo.getAll(null).size());
+        assertEquals(size + 4, courseRepo.getAll(null).size());
         
         Course toFind = new Course();
         toFind.setName("One");
         
-        List<Course> results = repo.getAll(toFind);
+        List<Course> results = courseRepo.getAll(toFind);
         assertEquals(1, results.size());
         
         toFind = new Course();
         toFind.setDescription("oneDesc");
-        results = repo.getAll(toFind);
+        results = courseRepo.getAll(toFind);
         assertEquals(2, results.size());
 
         toFind = new Course();
         toFind.setName("x");
-        results = repo.getAll(toFind);
+        results = courseRepo.getAll(toFind);
         assertEquals(0, results.size());
     }
     
     @Test
     public void testCRUD() {
-        int size = repo.getAll(null).size();
+        int size = courseRepo.getAll(null).size();
         Course c = new Course("Name222", "Desc");
-        repo.create(c);
-        assertEquals(size + 1, repo.getAll(null).size());
+        courseRepo.create(c);
+        assertEquals(size + 1, courseRepo.getAll(null).size());
         
-        Course c1 = repo.getById(c.getId());
+        Course c1 = courseRepo.getById(c.getId());
         assertEquals(c1.getId(), c.getId());
         long id = c1.getId();
         
         Course testCourse= new Course();
         testCourse.setName("Name222");
-        assertEquals(c1, repo.getAll(testCourse).get(0));
+        assertEquals(c1, courseRepo.getAll(testCourse).get(0));
         
-        boolean isDeleted = repo.delete(id);
+        boolean isDeleted = courseRepo.delete(id);
         assertTrue(isDeleted);
-        assertEquals(size, repo.getAll(null).size());
+        assertEquals(size, courseRepo.getAll(null).size());
         
-        assertEquals(0,repo.getAll(testCourse).size());
+        assertEquals(0,courseRepo.getAll(testCourse).size());
         
     }
     
@@ -207,11 +216,11 @@ public class CourseRepositoryTest {
         Course courseProg = new Course("Programmering", "programmering er kult");
 
         // adding course
-        courseProg.setId(repo.create(courseProg));
+        courseProg.setId(courseRepo.create(courseProg));
     
-        assertNotSame(0, repo.getAll(null).size());
+        assertNotSame(0, courseRepo.getAll(null).size());
         
         // deleting courses
-        repo.delete(courseProg.getId());
+        courseRepo.delete(courseProg.getId());
     }
 }
