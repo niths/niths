89b9addd02c16145391cdf5a9153fdfe442d3bd0
Bendik Rostad <bendikr@gmail.com>
2012-03-15 11:00:22 +0100

    Added security to subject controller

--- src/main/java/no/niths/application/rest/SubjectControllerImpl.java
+++ src/main/java/no/niths/application/rest/SubjectControllerImpl.java
@@ -4,6 +4,7 @@ import no.niths.application.rest.interfaces.SubjectController;
 import no.niths.application.rest.lists.ListAdapter;
 import no.niths.application.rest.lists.SubjectList;
 import no.niths.common.AppConstants;
+import no.niths.common.SecurityConstants;
 import no.niths.domain.Subject;
 import no.niths.services.interfaces.GenericService;
 import no.niths.services.interfaces.SubjectService;
@@ -11,7 +12,10 @@ import no.niths.services.interfaces.SubjectService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Controller for subjects
@@ -29,6 +33,33 @@ public class SubjectControllerImpl extends AbstractRESTControllerImpl<Subject> i
 
 	private SubjectList subjectList = new SubjectList();
 
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@PreAuthorize(SecurityConstants.ADMIN_AND_SR)
+	public void create(@RequestBody Subject domain) {
+		super.create(domain);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@PreAuthorize(SecurityConstants.ADMIN_AND_SR)
+	public void hibernateDelete(@PathVariable long id) {
+		super.hibernateDelete(id);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@PreAuthorize(SecurityConstants.ADMIN_AND_SR)
+	public void update(@RequestBody Subject domain) {
+		super.update(domain);
+	}
+	
 	/**
 	 * {@inheritDoc}
 	 */
