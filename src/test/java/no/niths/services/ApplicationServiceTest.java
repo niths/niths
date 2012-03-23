daf72acac96efcfb30fde446968eeef931c58e90
Bendik Rostad <bendikr@gmail.com>
2012-03-23 10:54:45 +0100

    Fixed a test + added a constr

--- src/test/java/no/niths/services/ApplicationServiceTest.java
+++ src/test/java/no/niths/services/ApplicationServiceTest.java
@@ -63,11 +63,11 @@ public class ApplicationServiceTest {
 	public void testDevRelation(){
 		int devSize = devService.getAll(null).size();
 		int appSize = service.getAll(null).size();
-		Developer d1 = new Developer();
+		Developer d1 = new Developer("Tha name");
 		devService.create(d1);
 		assertEquals(devSize + 1, devService.getAll(null).size());
-		Application a1 = new Application();
-		Application a2 = new Application();
+		Application a1 = new Application("Heilain");
+		Application a2 = new Application("Segkland");
 		service.create(a1);
 		service.create(a2);
 		assertEquals(appSize + 2, service.getAll(null).size());
