323638e131e62f5e19c21a6afc1942028df5da43
AndreKristensen <andre.kristensen@lyse.net>
2012-04-10 16:56:51 +0200

    Fixed exception handling in JSON and XML adapter

--- src/main/java/no/niths/domain/adapter/XmlDateAdapter.java
+++ src/main/java/no/niths/domain/adapter/XmlDateAdapter.java
@@ -1,24 +1,38 @@
 package no.niths.domain.adapter;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.niths.application.rest.exception.CustomParseException;
 import no.niths.common.AppConstants;
 
 public class XmlDateAdapter extends XmlAdapter<String,Date>{
 
-	
+	private Logger logger = LoggerFactory
+			.getLogger(XmlDateAdapter.class);
 	private DateFormat df = new SimpleDateFormat(AppConstants.DATE_FORMAT);  
 	@Override
-	public Date unmarshal(String date) throws Exception {
-		return df.parse(date);
+	public Date unmarshal(String date){
+		
+		try {
+			return df.parse(date);
+		} catch (ParseException px) {
+			logger.error(px.getMessage(),px);
+			throw new CustomParseException("Invalid syntacs! Valid syntax : "
+					+ AppConstants.DATE_FORMAT + " ErrorOffset:"
+					+ px.getErrorOffset());
+		}
 	}
 
 	@Override
-	public String marshal(Date date) throws Exception {
+	public String marshal(Date date){
 		return df.format(date);
 	}
 
