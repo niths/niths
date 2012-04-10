e85c36b12a9afb76b8ed89689a7569abfaf76828
Andre Kristensen <andre.kristensen@lyse.net>
2012-04-10 20:23:46 +0200

    Code cleanup removed unused imports

--- src/main/java/no/niths/external/QRCodeDecoder.java
+++ src/main/java/no/niths/external/QRCodeDecoder.java
@@ -3,22 +3,15 @@ package no.niths.external;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.util.Hashtable;
 
 import javax.imageio.ImageIO;
 
-import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.DecodeHintType;
 import com.google.zxing.MultiFormatReader;
-import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.Result;
-import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.GlobalHistogramBinarizer;
 import com.google.zxing.common.HybridBinarizer;
 
 
