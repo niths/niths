b190ad2eb5e89a8c8f777bd325c5315147f10822
Bendik Rostad <bendikr@gmail.com>
2012-04-12 14:22:07 +0200

    Added @NotNull to app.title + tests of dev controller addApp/removeApp

--- src/test/java/no/niths/application/rest/DeveloperControllerTest.java
+++ src/test/java/no/niths/application/rest/DeveloperControllerTest.java
@@ -3,11 +3,14 @@ package no.niths.application.rest;
 
 import static org.junit.Assert.assertEquals;
 import no.niths.application.rest.exception.ObjectNotFoundException;
+import no.niths.application.rest.interfaces.ApplicationController;
 import no.niths.application.rest.interfaces.DeveloperController;
 import no.niths.common.config.HibernateConfig;
 import no.niths.common.config.TestAppConfig;
+import no.niths.domain.Application;
 import no.niths.domain.Developer;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,35 @@ public class DeveloperControllerTest {
 	@Autowired 
 	private DeveloperController controller;
 	
+	@Autowired
+	private ApplicationController aController;
+	
+	@Test
+	public void addApp(){
+		int size = 0;
+		try{
+			size = controller.getAll(new Developer()).size();
+		}catch(ObjectNotFoundException e){}
+		
+		Developer dev = new Developer("Per", "person@nith.no");
+		controller.create(dev);
+		assertEquals(size + 1, controller.getAll(new Developer()).size());
+		
+		Application app = new Application("The title");
+		aController.create(app);
+		
+		Developer fetched = controller.getById(dev.getId());
+		Application aFetched = aController.getById(app.getId());
+		assertEquals(true, fetched.getApps().isEmpty());
+		controller.addApp(fetched.getId(), aFetched.getId());
+		fetched = controller.getById(dev.getId());
+		assertEquals(1, fetched.getApps().size());
+		
+		controller.removeApp(fetched.getId(), aFetched.getId());
+		fetched = controller.getById(dev.getId());
+		assertEquals(true, fetched.getApps().isEmpty());
+	}
+
 	@Test
 	public void testController(){
 		int size = 0;
@@ -29,8 +61,9 @@ public class DeveloperControllerTest {
 		}catch(ObjectNotFoundException e){}
 		
 		
-		Developer dev = new Developer("Name");
+		Developer dev = new Developer();
 		dev.setEmail("mail@mail.com");
+		dev.setName("DevName");
 		controller.create(dev);
 		
 		assertEquals(size + 1, controller.getAll(new Developer()).size());
