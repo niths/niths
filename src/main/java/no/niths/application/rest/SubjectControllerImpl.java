44e56668c761aded592d2c93687a105ebb0fe62e
Liv Kolaas <liv.kolaas@gmail.com>
2012-04-12 15:21:40 +0200

    Added add/removeRoom too SubjectController

--- src/main/java/no/niths/application/rest/SubjectControllerImpl.java
+++ src/main/java/no/niths/application/rest/SubjectControllerImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import no.niths.application.rest.exception.DuplicateEntryCollectionException;
 import no.niths.application.rest.exception.NotInCollectionException;
+import no.niths.application.rest.exception.ObjectNotFoundException;
 import no.niths.application.rest.interfaces.SubjectController;
 import no.niths.application.rest.lists.ListAdapter;
 import no.niths.application.rest.lists.SubjectList;
@@ -12,7 +13,9 @@ import no.niths.common.SecurityConstants;
 import no.niths.common.ValidationHelper;
 import no.niths.domain.Student;
 import no.niths.domain.Subject;
+import no.niths.domain.location.Room;
 import no.niths.services.interfaces.GenericService;
+import no.niths.services.interfaces.RoomService;
 import no.niths.services.interfaces.StudentService;
 import no.niths.services.interfaces.SubjectService;
 
@@ -46,60 +49,109 @@ public class SubjectControllerImpl extends AbstractRESTControllerImpl<Subject>
 	@Autowired
 	private StudentService studentService;
 
+    @Autowired
+    private RoomService roomService;
+
 	private SubjectList subjectList = new SubjectList();
 
 	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	@PreAuthorize(SecurityConstants.ADMIN_AND_SR)
-	@RequestMapping(value = "add/tutor/{subjectId}/{studentId}", method = RequestMethod.PUT)
-	@ResponseStatus(value = HttpStatus.OK, reason = "Tutor added to subject")
-	public void addTutor(@PathVariable Long subjectId,
-			@PathVariable Long studentId) {
-		Subject subject = service.getById(subjectId);
-		ValidationHelper.isObjectNull(subject, "Subject not found");
+     * {@inheritDoc}
+     */
+    @Override
+    @PreAuthorize(SecurityConstants.ADMIN_AND_SR)
+    @RequestMapping(value = "add/tutor/{subjectId}/{studentId}", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.OK, reason = "Tutor added to subject")
+    public void addTutor(@PathVariable Long subjectId,
+                         @PathVariable Long studentId) {
+        Subject subject = service.getById(subjectId);
+        ValidationHelper.isObjectNull(subject, "Subject not found");
 
-		Student student = studentService.getById(studentId);
-		ValidationHelper.isObjectNull(student, "Student not found");
+        Student student = studentService.getById(studentId);
+        ValidationHelper.isObjectNull(student, "Student not found");
 
-		if (!subject.getTutors().contains(student)) {
-			subject.getTutors().add(student);
-			service.update(subject);
-		} else {
-			throw new DuplicateEntryCollectionException(
-					"Tutor is already added to the subject");
-		}
-	}
+        if (!subject.getTutors().contains(student)) {
+            subject.getTutors().add(student);
+            service.update(subject);
+        } else {
+            throw new DuplicateEntryCollectionException(
+                    "Tutor is already added to the subject");
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	@PreAuthorize(SecurityConstants.ADMIN_AND_SR)
-	@RequestMapping(value = "remove/tutor/{subjectId}/{studentId}", method = RequestMethod.PUT)
-	@ResponseStatus(value = HttpStatus.OK, reason = "Tutor removed to subject")
-	public void removeTutor(@PathVariable Long subjectId,
-			@PathVariable Long studentId) {
-		Subject subject = service.getById(subjectId);
-		ValidationHelper.isObjectNull(subject, "Subject not found");
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @PreAuthorize(SecurityConstants.ADMIN_AND_SR)
+    @RequestMapping(value = "remove/tutor/{subjectId}/{studentId}", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.OK, reason = "Tutor removed to subject")
+    public void removeTutor(@PathVariable Long subjectId,
+                            @PathVariable Long studentId) {
+        Subject subject = service.getById(subjectId);
+        ValidationHelper.isObjectNull(subject, "Subject not found");
 
-		boolean isRemoved = false;
-		for (int i = 0; i < subject.getTutors().size(); i++) {
-			if (subject.getTutors().get(i).getId() == studentId) {
-				subject.getTutors().remove(i);
-				isRemoved = true;
-			}
-		}
+        boolean isRemoved = false;
+        for (int i = 0; i < subject.getTutors().size(); i++) {
+            if (subject.getTutors().get(i).getId() == studentId) {
+                subject.getTutors().remove(i);
+                isRemoved = true;
+            }
+        }
 
-		if (isRemoved) {
-			service.update(subject);
-			logger.debug("Tutor removed from subject " + subject.getName());
-		} else {
-			throw new NotInCollectionException("Student is not a tutor");
-		}
+        if (isRemoved) {
+            service.update(subject);
+            logger.debug("Tutor removed from subject " + subject.getName());
+        } else {
+            throw new NotInCollectionException("Student is not a tutor");
+        }
 
-	}
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @PreAuthorize(SecurityConstants.ADMIN_AND_SR)
+    @RequestMapping(value = "add/room/{subjectId}/{roomId}", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.OK, reason = "Room added to subject")
+    public void addRoom(@PathVariable Long subjectId,
+                         @PathVariable Long roomId) {
+        Subject subject = service.getById(subjectId);
+        ValidationHelper.isObjectNull(subject, "Subject not found");
+
+        Room room = roomService.getById(roomId);
+        ValidationHelper.isObjectNull(room, "Room not found");
+
+        subject.setRoom(room);
+        service.update(subject);
+        logger.debug("Subject updated");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @PreAuthorize(SecurityConstants.ADMIN_AND_SR)
+    @RequestMapping(value = "remove/room/{subjectId}", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.OK, reason = "Room removed from subject")
+    public void removeRoom(@PathVariable Long subjectId) {
+        Subject subject = service.getById(subjectId);
+        ValidationHelper.isObjectNull(subject, "Subject not found");
+
+        boolean isRemoved = false;
+        if (subject.getRoom() != null) {
+            subject.setRoom(null);
+            isRemoved = true;
+        }
+
+        if (isRemoved) {
+            service.update(subject);
+            logger.debug("Room removed from subject " + subject.getName());
+        } else {
+            throw new ObjectNotFoundException("Room not found");
+        }
+
+    }
 
 	@Override
 	public Subject getById(@PathVariable Long id) {
