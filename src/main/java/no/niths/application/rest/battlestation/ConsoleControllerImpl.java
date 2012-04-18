ca72a2d75736b7e1dd41090235c90970e59948ea
AndreKristensen <andre.kristensen@lyse.net>
2012-04-18 12:53:08 +0200

    remove unused fields

--- src/main/java/no/niths/application/rest/battlestation/ConsoleControllerImpl.java
+++ src/main/java/no/niths/application/rest/battlestation/ConsoleControllerImpl.java
@@ -7,12 +7,8 @@ import no.niths.application.rest.lists.ListAdapter;
 import no.niths.common.AppConstants;
 import no.niths.domain.battlestation.Console;
 import no.niths.services.battlestation.interfaces.ConsoleService;
-import no.niths.services.battlestation.interfaces.GameService;
-import no.niths.services.battlestation.interfaces.LoanService;
 import no.niths.services.interfaces.GenericService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -26,69 +22,62 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  */
 @Controller
 @RequestMapping(AppConstants.CONSOLES)
-public class ConsoleControllerImpl extends AbstractRESTControllerImpl<Console> implements ConsoleController {
+public class ConsoleControllerImpl extends AbstractRESTControllerImpl<Console>
+		implements ConsoleController {
 
-    private static final Logger logger = LoggerFactory
-            .getLogger(ConsoleControllerImpl.class);
+	@Autowired
+	private ConsoleService consoleService;
 
-    @Autowired
-    private ConsoleService consoleService;
+	private ConsoleList consoleList = new ConsoleList();
 
-    @Autowired
-    private GameService gameService;
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@RequestMapping(value = "{consoleId}/add/game/{gameId}", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.OK, reason = "Game Added")
+	public void addGame(@PathVariable Long consoleId, @PathVariable Long gameId) {
+		consoleService.addGame(consoleId, gameId);
+	}
 
-    @Autowired
-    private LoanService loanService;
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@RequestMapping(value = "{consoleId}/remove/game/{gameId}", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.OK, reason = "Game Removed")
+	public void removeGame(@PathVariable Long consoleId,
+			@PathVariable Long gameId) {
+		consoleService.removeGame(consoleId, gameId);
+	}
 
-    private ConsoleList consoleList = new ConsoleList();
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@RequestMapping(value = "{consoleId}/add/loan/{loanId}", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.OK, reason = "Loan Added")
+	public void addLoan(@PathVariable Long consoleId, @PathVariable Long loanId) {
+		consoleService.addLoan(consoleId, loanId);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @RequestMapping(value = "{consoleId}/add/game/{gameId}", method = RequestMethod.PUT)
-    @ResponseStatus(value = HttpStatus.OK, reason = "Game Added")
-    public void addGame(@PathVariable Long consoleId, @PathVariable Long gameId) {
-        consoleService.addGame(consoleId, gameId);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@RequestMapping(value = "{consoleId}/remove/loan", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.OK, reason = "Loan Removed")
+	public void removeLoan(@PathVariable Long consoleId) {
+		consoleService.removeLoan(consoleId);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @RequestMapping(value = "{consoleId}/remove/game/{gameId}", method = RequestMethod.PUT)
-    @ResponseStatus(value = HttpStatus.OK, reason = "Game Removed")
-    public void removeGame(@PathVariable Long consoleId, @PathVariable Long gameId) {
-        consoleService.removeGame(consoleId, gameId);
-    }
+	@Override
+	public GenericService<Console> getService() {
+		return consoleService;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @RequestMapping(value = "{consoleId}/add/loan/{loanId}", method = RequestMethod.PUT)
-    @ResponseStatus(value = HttpStatus.OK, reason = "Loan Added")
-    public void addLoan(@PathVariable Long consoleId, @PathVariable Long loanId) {
-        consoleService.addLoan(consoleId, loanId);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @RequestMapping(value = "{consoleId}/remove/loan", method = RequestMethod.PUT)
-    @ResponseStatus(value = HttpStatus.OK, reason = "Loan Removed")
-    public void removeLoan(@PathVariable Long consoleId) {
-        consoleService.removeLoan(consoleId);
-    }
-
-    @Override
-    public GenericService<Console> getService() {
-        return consoleService;
-    }
-
-    @Override
-    public ListAdapter<Console> getList() {
-        return consoleList;
-    }
+	@Override
+	public ListAdapter<Console> getList() {
+		return consoleList;
+	}
 }
