d4218cdaa24ae60ca2d9c0e7e23a7b46c75cdcae
whirlwin <whirlwin90@gmail.com>
2012-04-25 14:10:29 +0200

    Added support for gettings students + roles.

--- src/main/java/no/niths/services/school/StudentServiceImpl.java
+++ src/main/java/no/niths/services/school/StudentServiceImpl.java
@@ -115,9 +115,10 @@ public class StudentServiceImpl extends AbstractGenericService<Student>
 	@Override
 	public List<Student> getStudentsAndRoles(Student s) {
 		List<Student> list = repo.getAll(s);
-		for (int i = 0; i < list.size(); i++) {
-			list.get(i).getRoles().size();
-		}
+		lazyFixer.fetchChildren(list);
+//		for (int i = 0; i < list.size(); i++) {
+//			list.get(i).getRoles().size();
+//		}
 
 		return list;
 	}
