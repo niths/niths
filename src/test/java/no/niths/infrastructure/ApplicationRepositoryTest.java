c71ff96e397e978f7cf01a4452077298b9064434
Bendik Rostad <bendikr@gmail.com>
2012-04-26 19:24:09 +0200

    Added getTopApps() (most requests) in repo + tests

--- src/test/java/no/niths/infrastructure/ApplicationRepositoryTest.java
+++ src/test/java/no/niths/infrastructure/ApplicationRepositoryTest.java
@@ -1,6 +1,9 @@
 package no.niths.infrastructure;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import no.niths.common.config.HibernateConfig;
 import no.niths.common.config.TestAppConfig;
 import no.niths.domain.developing.Application;
@@ -37,6 +40,24 @@ public class ApplicationRepositoryTest {
 		assertEquals("title", appRepo.getById(a.getId()).getTitle());
 		
 		assertEquals(true, appRepo.delete(a.getId()));
+	}
+	
+	@Test
+	public void getTopApps(){
+		Application a1 = new Application("application xzy");
+		Application a2 = new Application("application xxs");
+		Application a3 = new Application("application xsy");
+		a1.setRequests(new Long(10));
+		a2.setRequests(new Long(20));
+		a3.setRequests(new Long(5));
+		appRepo.create(a1);
+		appRepo.create(a2);
+		appRepo.create(a3);
+		
+		List<Application> all = appRepo.getTopApps(5);
+		assertEquals(all.get(0), a2);
+		assertEquals(all.get(1), a1);
+		assertEquals(all.get(2), a3);
 	}
 
 }
