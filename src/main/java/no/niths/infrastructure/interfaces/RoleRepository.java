2560e768019a7abdab3ade78714fc2fa431f5058
Bendik Rostad <bendikr@gmail.com>
2012-04-26 13:50:17 +0200

    Added javadoc

--- src/main/java/no/niths/infrastructure/interfaces/RoleRepository.java
+++ src/main/java/no/niths/infrastructure/interfaces/RoleRepository.java
@@ -2,7 +2,10 @@ package no.niths.infrastructure.interfaces;
 
 import no.niths.domain.security.Role;
 
-
+/**
+ * Repository class for Role
+ * 
+ */
 public interface RoleRepository extends GenericRepository<Role>{
 
 }
