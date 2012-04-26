45400fe698708c06329cf9ce9e32cbb0bea06e48
Bendik Rostad <bendikr@gmail.com>
2012-04-26 14:45:38 +0200

    Added javadoc

--- src/main/java/no/niths/domain/Domain.java
+++ src/main/java/no/niths/domain/Domain.java
@@ -2,7 +2,15 @@ package no.niths.domain;
 
 import java.io.Serializable;
 
-
+/**
+ * Interface for all Domain classes
+ * <p>
+ * All new domains must implement this interface
+ * to take advantage of the generic repository,
+ * service and controller classes
+ * </p>
+ *
+ */
 public interface Domain extends Serializable{
 
 	void setId(Long id);
