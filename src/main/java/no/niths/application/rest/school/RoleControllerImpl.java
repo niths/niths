bb995e7a13a641a10f761ab8fe85c85182a1bf57
Bendik Rostad <bendikr@gmail.com>
2012-05-06 10:49:20 +0200

    Added security to role controller(Why removed?)

--- src/main/java/no/niths/application/rest/school/RoleControllerImpl.java
+++ src/main/java/no/niths/application/rest/school/RoleControllerImpl.java
@@ -1,15 +1,19 @@
 package no.niths.application.rest.school;
 
+import javax.servlet.http.HttpServletResponse;
+
 import no.niths.application.rest.AbstractRESTControllerImpl;
 import no.niths.application.rest.lists.ListAdapter;
 import no.niths.application.rest.lists.school.RoleList;
 import no.niths.application.rest.school.interfaces.RoleController;
 import no.niths.common.constants.DomainConstantNames;
+import no.niths.common.constants.SecurityConstants;
 import no.niths.domain.school.Role;
 import no.niths.services.interfaces.GenericService;
 import no.niths.services.school.interfaces.RoleService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -29,6 +33,25 @@ public class RoleControllerImpl extends AbstractRESTControllerImpl<Role>
 	private RoleService roleService;
 
 	private RoleList roleList = new RoleList();
+	
+	@Override
+	@PreAuthorize(SecurityConstants.ONLY_ADMIN)
+	public void create(Role domain, HttpServletResponse res) {
+		super.create(domain, res);
+	}
+	
+	@Override
+	@PreAuthorize(SecurityConstants.ONLY_ADMIN)
+	public void update(Role domain) {
+		super.update(domain);
+	}
+	
+	@Override
+	@PreAuthorize(SecurityConstants.ONLY_ADMIN)
+	public void delete(long id) {
+		super.delete(id);
+	}
+	
 
 	/**
 	 * {@inheritDoc}
