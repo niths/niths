947fc20717d58603b7305ebffa4c17e6e6812765
Bendik Rostad <bendikr@gmail.com>
2012-05-06 10:51:00 +0200

    Removed security(I see why they were removed...)

--- src/main/java/no/niths/application/rest/APIEventControllerImpl.java
+++ src/main/java/no/niths/application/rest/APIEventControllerImpl.java
@@ -1,18 +1,14 @@
 package no.niths.application.rest;
 
-import javax.servlet.http.HttpServletResponse;
-
 import no.niths.application.rest.interfaces.APIEventController;
 import no.niths.application.rest.lists.APIEventList;
 import no.niths.application.rest.lists.ListAdapter;
 import no.niths.common.constants.DomainConstantNames;
-import no.niths.common.constants.SecurityConstants;
 import no.niths.domain.APIEvent;
 import no.niths.services.interfaces.APIEventService;
 import no.niths.services.interfaces.GenericService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 /**
@@ -29,24 +25,6 @@ public class APIEventControllerImpl extends AbstractRESTControllerImpl<APIEvent>
 	private APIEventService service;
 
 	private APIEventList subjectList = new APIEventList();
-	
-	@Override
-	@PreAuthorize(SecurityConstants.ONLY_ADMIN)
-	public void update(APIEvent domain) {
-		super.update(domain);
-	}
-	
-	@Override
-	@PreAuthorize(SecurityConstants.ONLY_ADMIN)
-	public void create(APIEvent domain, HttpServletResponse res) {
-		super.create(domain, res);
-	}
-	
-	@Override
-	@PreAuthorize(SecurityConstants.ONLY_ADMIN)
-	public void delete(long id) {
-		super.delete(id);
-	}
 	
 	/**
 	 * {@inheritDoc}
